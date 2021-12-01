5c815f4867846d2d8fb90bb333ee3b685512d23c
Tomas Loesch <t224991@dac.unicamp.br>
2021-11-30 21:32:08 -0300

    mensagem de commit

--- ProjetoFinal/src/com/unicamp/mc322/Animacoes/Pokemon.java
+++ ProjetoFinal/src/com/unicamp/mc322/Animacoes/Pokemon.java
@@ -12,14 +12,79 @@ public class Pokemon {
 	private ArrayList<Type> listType;
 	private Dices dicesDistance = new Dices(1, 4);
 	private Dices dicesDifficulty = new Dices(2, 4);
+	private String type1;
+	private String type2;
 	
-	public Pokemon (Position ponto) {
-		this.ponto=ponto;
+	public Pokemon (Position ponto, String name, double hp, double attack, double defense, String type1, String type2) {
+		this.ponto = ponto;
+		this.name = name;
+		this.hp = hp;
+		this.attack = attack;
+		this.defense = defense;
+		this.type1 = type1;
+		this.type2 = type2;
 	}
 	
-	private void Captura() {
+	
+	public static Pokemon gerarPokemon(Position ponto) {
+		String[][] pokemons = {
+				{"Pikachu", "10", "10", "10", "electric", ""},
+				{"Squirtle", "10", "10", "10", "water", ""},
+				{"Charizard", "10", "10", "10", "fire", ""}
+		};
+		
+		Aleatorio aleatorio = new Aleatorio();
+		int pokemonPos = aleatorio.getNumber(pokemons.length);
+		String [] pokemon = pokemons[pokemonPos];
+		
+		return new Pokemon(
+				ponto,
+				pokemon[0], 
+				Double.parseDouble(pokemon[1]), 
+				Double.parseDouble(pokemon[2]), 
+				Double.parseDouble(pokemon[3]),
+				pokemon[4], pokemon[5]
+		);
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	public double getHp() {
+		return hp;
+	}
+	
+	public double getAttack() {
+		return attack;
+	}
+	
+	public double getDefense() {
+		return defense;
+	}
+	
+	public String getType1() {
+		return type1;
+	}
+	
+	public String getType2() {
+		return type2;
+	}
+	
+	public void setHp(double hp) {
+		this.hp = hp;
+	}
+	
+	public void setAttack(double attack) {
+		this.attack = attack;
+	}
+	
+	public void setDefense(double defense) {
+		this.defense = defense;
+	}
+	
+	public void Captura() {
 		int d = dicesDistance.PlayDices();
 		int k = dicesDifficulty.PlayDices();
-	
 	}
 }
